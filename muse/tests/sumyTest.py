bf17b99a3ceda80c7d814e6a09ab417f9b8fe84d
Hector <hector.david.1987@gmail.com>
2024-09-03 15:13:18 +0100

    adding sumy test

--- muse/tests/sumyTest.py
+++ muse/tests/sumyTest.py
@@ -7,6 +7,7 @@
 import unittest
 import sys
 import os
+import nltk
 
 try:
         home = os.environ["MUSE_HOME"]
@@ -20,6 +21,8 @@ from muse.system.extractive.sumyMuse import Sumy
 class TestGensim(unittest.TestCase):
 
     def test_summarize(self):
+        nltk.download('punkt')
+        nltk.download('punkt_tab')
         text = "This is a long text that needs to be summarized. It is very long and boring. I am trying to make it shorter. Maybe we can try to write something a little bit longer."
         sumy = Sumy(text)
         smry = sumy.summarize()  
