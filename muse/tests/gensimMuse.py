4983b85f887e66ef2f60d0063129429ab8113822
Hector <hector.david.1987@gmail.com>
2024-09-03 14:58:25 +0100

    fixin test

--- muse/tests/gensimMuse.py
+++ muse/tests/gensimMuse.py
@@ -18,19 +18,18 @@ sys.path.append(home)
 # from muse.system.extractive.gensimMuse import Gensim
 
 class TestGensim(unittest.TestCase):
-        def test_router(self):
-                self.assertEqual(home, "/home/user/muse")
-#     def test_summarize(self):
-#         text = "This is a long text that needs to be summarized. It is very long and boring. I am trying to make it shorter. Maybe we can try to write something a little bit longer."
-#         gensim = Gensim(text)
-#         summary = gensim.summarize()
-#         self.assertEqual(summary, "This is a long text that needs to be summarized.")
 
-#     def test_get_keywords(self):
-#         text = "This is a long text that needs to be summarized. It is very long and boring. I am trying to make it shorter. Maybe we can try to write something a little bit longer."
-#         gensim = Gensim(text)
-#         keywords = gensim.get_keywords()
-#         self.assertEqual(keywords, "long text")
+    def test_summarize(self):
+        text = "This is a long text that needs to be summarized. It is very long and boring. I am trying to make it shorter. Maybe we can try to write something a little bit longer."
+        gensim = Gensim(text)
+        summary = gensim.summarize()
+        self.assertEqual(summary, "This is a long text that needs to be summarized.")
 
-# if __name__ == '__main__':
-#     unittest.main()
+    def test_get_keywords(self):
+        text = "This is a long text that needs to be summarized. It is very long and boring. I am trying to make it shorter. Maybe we can try to write something a little bit longer."
+        gensim = Gensim(text)
+        keywords = gensim.get_keywords()
+        self.assertEqual(keywords, "long text")
+
+if __name__ == '__main__':
+    unittest.main()
