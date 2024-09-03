bce2a3acbd21c2e3cb3887da8f90892626ee6afd
Hector <hector.david.1987@gmail.com>
2024-09-03 12:31:22 +0100

    adding the first example

---
+++ muse/system/extractive/sumyMuse.py
@@ -0,0 +1,26 @@
+#An example of an object using sumy to summarize text
+# This is a simple example of how to use sumy to summarize text
+
+import nltk
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+
+class Sumy:
+    def __init__(self, text):
+        self.text = text
+
+    def summarize(self):
+        parser = PlaintextParser.from_string(self.text, Tokenizer("english"))
+        summarizer = LsaSummarizer()
+        summary = summarizer(parser.document, 3)
+        return summary
+
+# Example usage
+text = "This is a long text that needs to be summarized. It is very long and boring. I am trying to make it shorter."
+sumy = Sumy(text)
+summary = sumy.summarize()
+for sentence in summary:
+    print(sentence)
+
+print(summary)
