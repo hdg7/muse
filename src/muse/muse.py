65ab3b4bdd499a28ef41b9c864b759600c57241d
Hector <hector.david.1987@gmail.com>
2024-09-10 12:01:20 +0100

    starting muse file

--- src/muse/muse.py
+++ src/muse/muse.py
@@ -1,11 +1,19 @@
+import data_manager
+import system
+import evaluation
+
 class Muse:
     def __init__(self):
         pass
 
     def dataManager(datatype,dataPath,dataLanguage):
-        pass
+        if(datatype == "single"):
+            this.dataManager = data_manager.document.Document(dataPath,dataLanguage)
+#        elif(datatype == "multi"):
 
     def system(systemList):
+        if(systemList == "gensim"):
+            this.system = system.extractive.gensim_connector.Gensim()
         pass
 
     def evSummary(evMetricsList):
