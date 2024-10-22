aea452ca9e636c78e141ad89b7b8baf115444f97
Aidan Dakhama <ai.dakhama@gmail.com>
2024-10-22 16:47:17 +0100

    add reinstall script

--- reinstall.bash
+++ reinstall.bash
@@ -1,8 +1,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CWD=$(pwd)
-cd $DIR
+cd "$DIR" || exit
 pip uninstall -y muse
 hatch build -t wheel
 LATEST_WHEEL=$(ls -t dist | grep '.whl' | head -n 1)
 pip install ./dist/"$LATEST_WHEEL"
-cd $CWD
+cd "$CWD" || exit
