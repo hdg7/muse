4c0b11e8a7059b3b9528cc259c5c1338312ce019
Hector <hector.david.1987@gmail.com>
2024-10-25 15:36:36 +0100

    adding spacy

--- src/muse/summarizer/extractive/__init__.py
+++ src/muse/summarizer/extractive/__init__.py
@@ -1,1 +1,2 @@
 from muse.summarizer.extractive.sumy_connector import Sumy
+from muse.summarizer.extractive.spacy_connector import Spacy
---
+++ tests/test_summarizers/test_spacy.py
@@ -0,0 +1,13 @@
+from muse.data_manager import Document
+from muse.summarizer import Spacy
+
+
+def test_summarize():
+    text = Document(
+        "This is a long text that needs to be summarized. It is very long and boring. I am trying to "
+        "make it shorter. Maybe we can try to write something a little bit longer."
+    )
+    spacy = Spacy({})
+    summary = spacy.summarize([text])
+    assert str(summary[0]) == "This is a long text that needs to be summarized. Maybe we can try to write something a little bit longer. It is very long and boring."
+
